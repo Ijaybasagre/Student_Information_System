8bc58c7908772b952ca32c6abdf413078c124e35
Ian Jay Basagre <71416469+Ijaybasagre@users.noreply.github.com>
2024-04-30 10:02:06 +0800

    initial commit

---
+++ src/main/java/com/projects/Student_Information_System/StudentInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.projects.Student_Information_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInformationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/Student_Information_System/StudentInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.Student_Information_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
